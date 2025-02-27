8f9c546a1ced4773134b40d131a1f5a175a57af8
Haruto Konishi <haruto.konishi@sens.sys.es.osaka-u.ac.jp>
2025-02-27 15:49:41 +0900

    add serial receive

--- M5StickC/MiniVibrator/src/main.cpp
+++ M5StickC/MiniVibrator/src/main.cpp
@@ -5,13 +5,22 @@
 void setup() {
     auto cfg = M5.config();
     M5.begin(cfg);
+    Serial.begin(115200);
     pinMode(OUTPUT_PIN, OUTPUT);
 }
 
 void loop() {
-    //OUTPUT_PINをHIGHとLOW交互に切り替える
-    digitalWrite(OUTPUT_PIN, HIGH);
-    delay(100);
-    digitalWrite(OUTPUT_PIN, LOW);
-    delay(500);
+    char receivedChar = '\0';  // 受信データを格納する変数（初期値はNULL）
+
+    if (Serial.available() > 0) {
+        receivedChar = Serial.read();  // 1文字読み取る
+    }
+
+    // 受信データに応じて条件分岐
+    if (receivedChar == 'X') {
+        digitalWrite(OUTPUT_PIN, LOW);
+    } else if (receivedChar == 'O') {
+        digitalWrite(OUTPUT_PIN, HIGH);
+    }
+    delay(50);
 }
